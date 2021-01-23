2d782a59d8b84e2422ff66c93bf2474b4a893fab
Lucas <desarrollolucasburroni@gmail.com>
2021-01-23 08:26:17 -0300

    agrege evento submit, mejor responsiv, contenido.

---
+++ contactApp.js
@@ -0,0 +1,21 @@
+const campo = document.querySelector('.campo'); 
+const form = document.querySelector('#formu');
+
+let nombre = "";
+campo.addEventListener("keyup", function(){
+  nombre = this.value;
+});
+const submi = (event) => {       
+     alert( `Gracias por tu mensaje ${nombre} !!`)
+     event.preventDefault(); 
+}
+ 
+document.addEventListener("DOMContentLoaded" , () => {
+
+    form.addEventListener('submit', submi);
+    event.preventDefault();
+
+  });
+
+
+    
